52f963ea57ca0902e7fb7313f0cfc42bfc0e45d8
Américo Valdazo <63754514+ValdazoAmerico@users.noreply.github.com>
2023-07-04 17:22:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+
+import streamlit as st
+
+from utils import chart, db
+
 import streamlit as st
 from streamlit_chat import message
 import streamlit_scrollable_textbox as stx
@@ -43,7 +49,7 @@ AI:"""
 # in more random responses, 
 # while a lower temperature will result in more predictable responses.
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 # Create the navigation bar for switching between tabs
 with col1:
     st.title("ÜmaBot 👩‍⚕️\nOpenAI GPT-3.5 🤖 + Langchain ⛓️ + Streamlit ")
@@ -821,5 +827,44 @@ Reposo: 24 horas
         st.subheader(f"Patient '{st.session_state['patient_uid'][0]}' Information")
         stx.scrollableTextbox(patient_data,height = 350)
 
+COMMENT_TEMPLATE_MD = """{} - {}
+> {}"""
 
-	
+
+def space(num_lines=1):
+    """Adds empty lines to the Streamlit app."""
+    for _ in range(num_lines):
+        st.write("")
+
+
+
+conn = db.connect()
+comments = db.collect(conn)
+
+with col3:
+ with st.expander("💬 Open comments"):
+        st.write("**Comments:**")
+        for index, entry in enumerate(comments.itertuples()):
+            st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
+
+            is_last = index == len(comments) - 1
+            is_new = "just_posted" in st.session_state and is_last
+            if is_new:
+              st.success("☝️ Your comment was successfully posted.")
+ 
+        space(2)
+
+		# Insert comment
+
+        st.write("**Add your own comment:**")
+        form = st.form("comment")
+        name = form.text_input("Name")
+        comment = form.text_area("Comment")
+        submit = form.form_submit_button("Add comment")
+
+        if submit:
+            date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            db.insert(conn, [[name, date, comment]])
+            if "just_posted" not in st.session_state:
+                st.session_state["just_posted"] = True
+            st.experimental_rerun()
