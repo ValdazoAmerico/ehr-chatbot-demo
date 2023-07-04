e9188dc857fd7f7e9bc4e9b19c037c9f8df11c4e
Américo Valdazo <63754514+ValdazoAmerico@users.noreply.github.com>
2023-07-04 17:24:28 -0300

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,6 @@
+SCOPE = "https://www.googleapis.com/auth/spreadsheets"
+SPREADSHEET_ID = "1ekD2Ax0oKaTSQHDqO5K2nMmrcWOHs9sxxBblQcyKObI"
+SHEET_NAME = "UT"
+GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
+COMMENT_TEMPLATE_MD = """{} - {}
+> {}"""
